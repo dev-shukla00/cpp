97aaa0e050e2853891d4e64ec6da79c8b95e6522
dev-shukla00 <immortal2077@duck.com>
2026-04-25 16:05:59 +0530

    added branch pseudo

---
+++ 0_index.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+cl
+
+int main() {
+    cout << "cosmos";
---
+++ 1_flowchart-pseudocode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main 
+
+
+
+
+// Homework Question-
+
+//calculate simple interest.
