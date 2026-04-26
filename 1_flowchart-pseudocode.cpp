9accead4af9e28a8d205b07d7d8c9627028e8ab2
dev-shukla00 <immortal2077@duck.com>
2026-04-26 19:01:49 +0530

    modified flowchart

--- 1_flowchart-pseudocode.cpp
+++ 1_flowchart-pseudocode.cpp
@@ -3,18 +3,31 @@ using namespace std;
 
 int main()
 {
-    //-firstcode
-    // string name = "cosmos";
-    // cout << "Hello " << name << "!" << endl;
+    // firstcode Hello Cosmos
+    string name = "cosmos";
+    cout << "\nHello " << name << "!\n"
+         << endl;
 
-    //-sum of two numbers
-    // int a, b;
-    // cout << "\nEnter first value: ";
-    // cin >> a;
-    // cout << "Enter sencond value: ";
-    // cin >> b;
-    // int sum = a + b;
-    // cout << "Result : " << sum << endl;
+    // area of square
+    int aos;
+    cout << "Enter a value to calculate area of square: ";
+    cin >> aos;
+
+    int area_of_square = aos * aos;
+    cout << "Area of Square: \n"
+         << area_of_square << endl;
+
+    // sum of two numbers
+    int value_one, value_two;
+
+    cout << "\nEnter first value to calculate sum of two no: ";
+    cin >> value_one;
+
+    cout << "Enter sencond value to calculate sum of two no: ";
+    cin >> value_two;
+
+    int sum = value_one + value_two;
+    cout << "Result : " << sum << endl;
 
     //-Homework Questions-
     // simple interest.
