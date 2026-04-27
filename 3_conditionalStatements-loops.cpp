c9caacf99cb73edd98f15bcfdac866c035533da5
dev-shukla00 <immortal2077@duck.com>
2026-04-27 23:17:11 +0530

    nested loop asterisk program

--- 3_conditionalStatements-loops.cpp
+++ 3_conditionalStatements-loops.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    // for loop, while loop, do while loop
+    // Nested loops
+
+    // int n = 3;
+    int n;
+    cout << "No of lines: ";
+    cin >> n;
+
+    // int m;
+    // cout << "No of stars: ";
+    // cin >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int m = i;
+        for (int j = 1; j <= m; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
