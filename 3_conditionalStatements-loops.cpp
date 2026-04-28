99368d9c0cc48db96e96128fd964bd8abdc8cdb3
dev-shukla00 <immortal2077@duck.com>
2026-04-28 16:20:35 +0530

    add sum of numbers divisible by three, and factorial of a number n

--- 3_conditionalStatements-loops.cpp
+++ 3_conditionalStatements-loops.cpp
@@ -4,25 +4,65 @@ using namespace std;
 int main()
 {
     // for loop, while loop, do while loop
-    // Nested loops
+    // // Nested loops
 
-    // int n = 3;
-    int n;
-    cout << "No of lines: ";
-    cin >> n;
+    // // int n = 3;
+    // int n;
+    // cout << "No of lines: ";
+    // cin >> n;
 
-    // int m;
-    // cout << "No of stars: ";
-    // cin >> m;
+    // // int m;
+    // // cout << "No of stars: ";
+    // // cin >> m;
 
-    for (int i = 1; i <= n; i++)
+    // for (size_t i = 1; i <= n; i++)
+    // {
+    //     int m = i;
+    //     for (int j = 1; j <= m; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Homework Questions
+    // Sum of all numbers from 1 to N which are  divisible by 3
+    int sumN;
+    int sum = 0;
+    cout << "Enter a number: ";
+    cin >> sumN;
+
+    for (size_t i = 1; i <= sumN; i++)
     {
-        int m = i;
-        for (int j = 1; j <= m; j++)
+        if (i % 3 == 0)
         {
-            cout << "*";
+            cout << i;
+            if (i + 3 <= sumN)
+            {
+                cout << " + ";
+            }
+            sum += i;
         }
-        cout << endl;
+    }
+    cout << " = " << sum << endl;
+
+    // Print Factorial of a number N.
+
+    int factN;
+    int sumfact = 1;
+    cout << "Enter a number: ";
+    cin >> factN;
+
+        for (size_t i = 1; i <= factN; i++)
+        {
+            cout << i;
+            if (i < factN)
+            {
+                cout << " * ";
+            }
+            sumfact = sumfact * i;
+        }
+        cout << " = " << sumfact << endl;
     }
     return 0;
 }
