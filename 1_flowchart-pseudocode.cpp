e1c13adaa01cecd0863c146c894cf668f40b6ecf
dev-shukla00 <immortal2077@duck.com>
2026-04-29 19:52:57 +0530

    add block in all files to separate from other code

--- 1_flowchart-pseudocode.cpp
+++ 1_flowchart-pseudocode.cpp
@@ -3,117 +3,123 @@ using namespace std;
 
 int main()
 {
-     // // firstcode Hello Cosmos
-     // string name = "cosmos";
-     // cout << "\nHello " << name << "!\n"
-     //      << endl;
 
-     // // area of square
-     // int side;
-     // cout << "Enter a value to calculate area of square: ";
-     // cin >> side;
-
-     // int area = side * side;
-     // cout << "Area of Square: \n"
-     //      << area << endl;
-
-     // // sum of two numbers
-     // int value_one, value_two;
-     // cout << "\nEnter first value: ";
-     // cin >> value_one;
-
-     // cout << "Enter second value: ";
-     // cin >> value_two;
-
-     // int sum = value_one + value_two;
-     // cout << "Result: " << sum << endl;
-
-     // // minimum of two numbers
-     // int num_one, num_two;
-     // cout << "Enter first number: ";
-     // cin >> num_one;
-
-     // cout << "Enter second number: ";
-     // cin >> num_two;
-
-     // if (num_one < num_two)
-     // {
-     //      cout << num_one << endl;
-     // }
-     // else if (num_one == num_two || num_two == num_one)
-     // {
-     //      cout << "The numbers are equal, Try Again!" << endl;
-     // }
-     // else
-     // {
-     //      cout << num_two << endl;
-     // }
-
-     // Is number odd or even!
-
-     // int odd_even_num;
-     // cout << "Enter a number: ";
-     // cin >> odd_even_num;
-
-     // if (odd_even_num % 2 == 0)
-     // {
-     //      cout << "Even Number!" << endl;
-     // }
-     // else if (odd_even_num % 2 == 1)
-     // {
-     //      cout << "Odd Number!" << endl;
-     // }
-     // else
-     // {
-     //      cout << "Try again with a number!" << endl;
-     // }
-
-     // Sum of numbers from 1 to n
-
-     // int n_inp;
-     // int count = 1;
-     // int sum = 0;
-
-     // cout << "Enter a number: ";
-     // cin >> n_inp;
-
-     // while (count <= n_inp)
-     // {
-     //      cout << count << " + ";
-     //      sum += count;
-     //      count++;
-     // }
-     // cout << " : " << sum << endl;
-
-     // Is prime number or not
-
-     int varPrime;
-     bool is_prime = true;
-     cout << "Enter a number: ";
-     cin >> varPrime;
-
-     for (size_t i = 2; i * i <= varPrime; i++)
-     // or we can also do this same this like below this, but above method is better fundamentaly.
-     //  for (size_t i = 2; i <= varPrime-1; i++)
      {
-          if (varPrime % i == 0)
+          // firstcode Hello Cosmos
+          string name = "cosmos";
+          cout << "\nHello " << name << "!\n"
+               << endl;
+     }
+     {
+          // area of square
+          int side;
+          cout << "Enter a value to calculate area of square: ";
+          cin >> side;
+
+          int area = side * side;
+          cout << "Area of Square: \n"
+               << area << endl;
+     }
+     {
+          // sum of two numbers
+          int value_one, value_two;
+          cout << "\nEnter first value: ";
+          cin >> value_one;
+
+          cout << "Enter second value: ";
+          cin >> value_two;
+
+          int sum = value_one + value_two;
+          cout << "Result: " << sum << endl;
+     }
+     {
+          // minimum of two numbers
+          int num_one, num_two;
+          cout << "Enter first number: ";
+          cin >> num_one;
+
+          cout << "Enter second number: ";
+          cin >> num_two;
+
+          if (num_one < num_two)
           {
-               is_prime = false;
-               break;
+               cout << num_one << endl;
+          }
+          else if (num_one == num_two || num_two == num_one)
+          {
+               cout << "The numbers are equal, Try Again!" << endl;
+          }
+          else
+          {
+               cout << num_two << endl;
           }
      }
-
-     if (is_prime == true)
      {
-          cout << "Prime Number" << endl;
-     }
-     else
-     {
-          cout << "Non-prime Number" << endl;
-     }
+          // Is number odd or even!
 
-     //-Homework Questions-
-     // simple interest.
+          int odd_even_num;
+          cout << "Enter a number: ";
+          cin >> odd_even_num;
+
+          if (odd_even_num % 2 == 0)
+          {
+               cout << "Even Number!" << endl;
+          }
+          else if (odd_even_num % 2 == 1)
+          {
+               cout << "Odd Number!" << endl;
+          }
+          else
+          {
+               cout << "Try again with a number!" << endl;
+          }
+     }
+     {
+          // Sum of numbers from 1 to n
+
+          int n_inp;
+          int count = 1;
+          int sum = 0;
+
+          cout << "Enter a number: ";
+          cin >> n_inp;
+
+          while (count <= n_inp)
+          {
+               cout << count << " + ";
+               sum += count;
+               count++;
+          }
+          cout << " : " << sum << endl;
+     }
+     {
+          // Is prime number or not
+
+          int varPrime;
+          bool is_prime = true;
+          cout << "Enter a number: ";
+          cin >> varPrime;
+
+          for (size_t i = 2; i * i <= varPrime; i++)
+          // or we can also do this same this like below this, but above method is better fundamentaly.
+          //  for (size_t i = 2; i <= varPrime-1; i++)
+          {
+               if (varPrime % i == 0)
+               {
+                    is_prime = false;
+                    break;
+               }
+          }
+
+          if (is_prime == true)
+          {
+               cout << "Prime Number" << endl;
+          }
+          else
+          {
+               cout << "Non-prime Number" << endl;
+          }
+     }
 
      return 0;
 }
--- 3_conditionalStatements-loops.cpp
+++ 3_conditionalStatements-loops.cpp
@@ -3,55 +3,61 @@ using namespace std;
 
 int main()
 {
-    // for loop, while loop, do while loop
-    // // Nested loops
-
-    // // int n = 3;
-    // int n;
-    // cout << "No of lines: ";
-    // cin >> n;
-
-    // // int m;
-    // // cout << "No of stars: ";
-    // // cin >> m;
-
-    // for (size_t i = 1; i <= n; i++)
-    // {
-    //     int m = i;
-    //     for (int j = 1; j <= m; j++)
-    //     {
-    //         cout << "*";
-    //     }
-    //     cout << endl;
-    // }
-
-    // Homework Questions
-    // Sum of all numbers from 1 to N which are  divisible by 3
-    int sumN;
-    int sum = 0;
-    cout << "Enter a number: ";
-    cin >> sumN;
-
-    for (size_t i = 1; i <= sumN; i++)
     {
-        if (i % 3 == 0)
+        // Types of loops are: for loop, while loop, do while loop
+
+        // Nested loops: it means loops inside loops
+
+        // int n = 3;
+        int n;
+        cout << "No of lines: ";
+        cin >> n;
+
+        // int m;
+        // cout << "No of stars: ";
+        // cin >> m;
+
+        for (size_t i = 1; i <= n; i++)
         {
-            cout << i;
-            if (i + 3 <= sumN)
+            int m = i;
+            for (int j = 1; j <= m; j++)
             {
-                cout << " + ";
+                cout << "*";
             }
-            sum += i;
+            cout << endl;
         }
     }
-    cout << " = " << sum << endl;
+    {
+        // **Homework Questions**
 
-    // Print Factorial of a number N.
+        // Sum of all numbers from 1 to N which are  divisible by 3
 
-    int factN;
-    int sumfact = 1;
-    cout << "Enter a number: ";
-    cin >> factN;
+        int sumN;
+        int sum = 0;
+
+        cout << "Enter a number: ";
+        cin >> sumN;
+
+        for (size_t i = 1; i <= sumN; i++)
+        {
+            if (i % 3 == 0)
+            {
+                cout << i;
+                if (i + 3 <= sumN)
+                {
+                    cout << " + ";
+                }
+                sum += i;
+            }
+        }
+        cout << " = " << sum << endl;
+
+        // Print Factorial of a number N.
+
+        int factN;
+        int sumfact = 1;
+        cout << "Enter a number: ";
+        cin >> factN;
 
         for (size_t i = 1; i <= factN; i++)
         {
