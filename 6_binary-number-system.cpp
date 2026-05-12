42eb3b6d2ab7b6ce5d928e2655cbea1dc38c232e
dev-shukla00 <immortal2077@duck.com>
2026-05-12 17:27:11 +0530

    dec to bin & bin to dec, one's and two's compliment

--- 6_binary-number-system.cpp
+++ 6_binary-number-system.cpp
@@ -24,13 +24,25 @@ int powOfTwo(int numPower)
 
     int ans = 1;
 
-    while (numPower > 0)
+    for (int i = 0; i < numPower; i++)
     {
-        for (int i = 0; i < numPower; i++)
-        {
-            ans *= 2;
-        }
-        break;
+        ans *= 2;
+    }
+    return ans;
+}
+
+int binToDec(int binNum)
+{
+    int pow = 1;
+    int ans = 0;
+
+    while (binNum > 0)
+    {
+        int rem = binNum % 10;
+        ans += rem * pow;
+        pow *= 2;
+
+        binNum = binNum / 10;
     }
     return ans;
 }
@@ -54,5 +66,14 @@ int main()
 
         cout << powOfTwo(numPower) << endl;
     }
+
+    { // Binary to decimal
+        int binNUm;
+        cout << "Enter a binary number: ";
+        cin >> binNUm;
+
+        cout << binToDec(binNUm) << endl;
+    }
+
     return 0;
 }
